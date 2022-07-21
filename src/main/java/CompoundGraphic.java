516e39d1702f019488e6e179d55cd2c12060b721
Overglitch_ <phmrlldz@gmail.com>
2022-07-21 02:53:42 -0500

    Update CompoundGraphic.java

--- src/main/java/CompoundGraphic.java
+++ src/main/java/CompoundGraphic.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class CompoundGraphic implements Graphic{
-    private ArrayList<Graphic> children=new ArrayList<>();
+    private final ArrayList<Graphic> children=new ArrayList<>();
     @Override
     public void move(int x,int y){
         for (Graphic g:children) g.move(x,y);
     }
     @Override
     public void draw(){
-        System.out.println("-------Start print Compound--------");
+        System.out.println("-------Start print Compound-------");
         for (Graphic g:children) g.draw();
-        System.out.println("--------End print Compound---------");
+        System.out.println("--------End print Compound--------");
     }
 
     public void add(Graphic graphic){
